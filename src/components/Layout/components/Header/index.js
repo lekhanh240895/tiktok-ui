f3f377d99414b8b1fa7d1fe455fe63ae35ad52f9
Lê Khánh <lekhanh240895@gmail.com>
2022-07-24 23:02:24 +0700

    Xây Dựng UI Phần Header Dự Án Tiktok

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,54 @@
 import React from 'react';
+import classnames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faSearch,
+    faPlus,
+    faSpinner,
+    faXmarkCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-    return <div>Header</div>;
+    const cx = classnames.bind(styles);
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <Link to="/" className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok Logo" />
+                </Link>
+
+                <form className={cx('search')}>
+                    <input
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                        type="text"
+                    />
+
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faXmarkCircle} />
+                    </button>
+
+                    <button className={cx('loading')}>
+                        <FontAwesomeIcon icon={faSpinner} />
+                    </button>
+
+                    <button type="submit" className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </form>
+
+                <div className={cx('actions')}>
+                    <button>
+                        <FontAwesomeIcon icon={faPlus} />
+                        Upload
+                    </button>
+
+                    <button>Login</button>
+                </div>
+            </div>
+        </header>
+    );
 }
