1841686dba22a7479d6ee69d4087f11359391f58
Lê Khánh <lekhanh240895@gmail.com>
2022-07-24 23:03:11 +0700

    Xây Dựng UI Phần Header Dự Án Tiktok

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from '../components/Header/index';
 import Sidebar from '../components/Sidebar/index';
+import styles from './DefaultLayout.module.scss';
+import classnames from 'classnames/bind';
 
 export default function DefaultLayout({ children }) {
+    const cx = classnames.bind(styles);
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
 
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
 
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
--- src/components/Layout/components/Sidebar/index.js
+++ src/components/Layout/components/Sidebar/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import styles from './Sidebar.module.scss';
+import classnames from 'classnames/bind';
 
 export default function Sidebar() {
-    return <div>Sidebar</div>;
+    const cx = classnames.bind(styles);
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('inner')}>Sidebar</div>
+        </div>
+    );
 }
